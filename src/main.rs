a5ac6cd6cee5b6b72066a334a4e13a5b899d5af9
CattoYT <twilsprk@gmail.com>
2024-11-07 22:12:26 +0000

    spicetify + spotx

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ fn main() {
     // create cmd and also add args
     let matches = Command::new("tltw")
         .arg(
+            Arg::new("removeall")
+            .long("removeall")
+            .help("Removes all tweaks from Spotify")
+            .action(clap::ArgAction::SetTrue),
+        )
+        .arg(
             Arg::new("spicetify")
                 .long("spicetify")
                 .help("Enable spicetify functionality")
@@ -30,6 +36,13 @@ fn main() {
 
     match std::env::consts::OS {
         "windows" => {
+
+            if matches.get_flag("removeall") {
+                println!("Removing tweaks...");
+                windows::remove_all_spotify_tweaks();
+                // https://raw.githubusercontent.com/SpotX-Official/SpotX/refs/heads/main/Uninstall.bat run this
+                return
+            }
             
             if matches.get_flag("spotx") {
                 println!("Adding spotx to Spotify");
@@ -40,7 +53,7 @@ fn main() {
             
             if matches.get_flag("spicetify") {
                 println!("Applying spicetify to Spotify");
-                windows::install_spicetify();
+                let _ = windows::install_spicetify();
             } else {
                 println!("Skipping spicetify installation");
             }
--- src/windows.rs
+++ src/windows.rs
@@ -1,27 +1,104 @@
-use std::{io::Error, process::Output};
+use std::{io::{BufRead, BufReader, Error}, process::{Output, Stdio}, thread};
+
+use std::io::Write;
 
 
 
 
-fn run_with_powershell(cmd: &str) -> Result<Output, Error> {
-    let spotx_cmd = std::process::Command::new(
+
+
+
+
+
+pub fn remove_all_spotify_tweaks(){
+    std::process::Command::new(
         "powershell")
         .arg("-Command")
-        .arg(cmd)
-        .output();
-        
-    
-
-    spotx_cmd
+        .arg("spicetify restore ; rmdir -r -fo $env:APPDATA\\spicetify; rmdir -r -fo $env:LOCALAPPDATA\\spicetify
+        ; Invoke-WebRequest -Uri \"https://raw.githubusercontent.com/SpotX-Official/SpotX/refs/heads/main/Uninstall.bat\" -OutFile \"$env:TEMP\\Uninstall.bat\"
+        & \"$env:TEMP\\Uninstall.bat\" ;
+        Remove-Item \"$env:TEMP\\Uninstall.bat\" -Force
+    ")
+        .stdin(Stdio::piped()) // Allow input
+        .output()
+        .expect("failed to exspecute process");
 }
 
+pub fn install_spicetify() -> Result<(), Error> {
+    let spicetify_dir = std::env::var("USERPROFILE").unwrap_or_else(|_| "Unknown Home".to_string()) + "\\AppData\\Local\\spicetify";
+
+    //check if spicetify is actually installed
+    match std::process::Command::new("spicetify").spawn() {
+        Ok(_) => println!("Executable found and started."),
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => {
+                println!("Spicetify not found. Installing...");
+
+                let mut spicetify = std::process::Command::new(
+                    "powershell")
+                    .arg("-Command")
+                    .arg("iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex")
+                    .stdin(Stdio::piped()) // Allow input
+                    .spawn()
+                    .expect("failed to exspecute process");
+                    
+                if let Some(ref mut stdin) = spicetify.stdin {
+                        // yeet y for the spicetify marketplace to be installed
+                    writeln!(stdin, "y").expect("Failed to write to stdin");
+                }
+
+                let result: Result<std::process::ExitStatus, Error> = spicetify.wait();
+                match result {
+                    Ok(_) => {
+                        println!("Spicetify installed successfully");
+                        return Ok(())
+                    
+                    }
+                        ,
+                    Err(_) =>  {
+                        println!("Spicetify failed! Rerunning with backup...");
+                        let mut spicetify_backup_cmd = std::process::Command::new(&spicetify_dir)
+                            .arg("backup")
+                            .arg("apply")
+
+                            .stdout(Stdio::piped())
+                            .spawn()
+                            .expect("failed to execute process");
+                
+                            let stdout = spicetify_backup_cmd.stdout.take();
+                
+                            let output_thread = thread::spawn(move || {
+                                let reader = BufReader::new(stdout.unwrap());
+                                let mut error_file = std::fs::File::create("spicetify_backup.log").unwrap();
+                                //thanks copilot
+                                error_file.write_all(reader.lines().map(|line| line.unwrap()).collect::<Vec<String>>().join("\n").as_bytes()).unwrap();
+                                
+                            });
+                            
+
+                
+                        let _ = spicetify_backup_cmd.wait().expect("Failed to wait for process");
+                        output_thread.join().expect("Output thread panicked");
+                    }
+                }
+            }
+            _ => println!("Other error occurred: {:?}", e)
+        },
+    }
+
+    println!("Running spicetify");
+
+
+    Err(Error::new(std::io::ErrorKind::Other, "Spicetify failed to install"))
+    
+}
 
 pub fn install_spotx() {
     let spotx_cmd = std::process::Command::new(
         "powershell")
         .arg("-Command")
         .arg("[Net.ServicePointManager]::SecurityProtocol = 3072; 
-        iex \"& { $(iwr -useb 'https://spotx-official.github.io/run.ps1') } -block_update_on\"")
+    iex \"& { $(iwr -useb 'https://spotx-official.github.io/run.ps1') } -sp-over -podcasts_off -block_update_on\"")
     .output()
     .expect("failed to execute process");
 
@@ -29,43 +106,6 @@ pub fn install_spotx() {
 
     print!("{}", spotx_out);
 
-}
-
-pub fn install_spicetify(){
-    
-    //check if spicetify is actually installed
-    match std::process::Command::new("spicetify").spawn() {
-        Ok(_) => println!("Executable found and started."),
-        Err(e) => match e.kind() {
-            std::io::ErrorKind::NotFound => {
-                println!("Spicetify not found. Installing...");
-                run_with_powershell("iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex").unwrap();}
-            _ => println!("Other error occurred: {:?}", e),
-        },
-    }
-
-    println!("Running spicetify");
-    let spicetify_cmd = std::process::Command::new("spicetify")
-    .arg("apply")
-    .output()
-    .expect("failed to execute process");
-
-    let spicetify_output = String::from_utf8_lossy(&spicetify_cmd.stdout);
-    print!("{}", spicetify_output);
-    // check if there is the "warning" substring inside of spicetify_output
-    if spicetify_output.contains("spicetify backup apply") {
-        println!("Spicetify failed! Rerunning with backup...");
-        let spicetify_backup_cmd = std::process::Command::new("spicetify")
-            .arg("backup")
-            .arg("apply")
-            .output()
-            .expect("failed to execute process");
-
-        print!("{}", String::from_utf8_lossy(&spicetify_backup_cmd.stdout));
-    } else {
-        println!("Spicetify has been applied successfully");
-    }
-    
 }
 
 pub fn install_soggify() {
@@ -80,12 +120,7 @@ pub fn install_soggify() {
                 println!("the fuck did you do");
                 panic!("the fuck?")
             } else {
-                std::process::Command::new(
-                    "[Net.ServicePointManager]::SecurityProtocol = 3072; 
-                    iex \"& { $(iwr -useb 'https://spotx-official.github.io/run.ps1') } -block_update_on\"")
-                .output()
-                .expect("failed to execute process"
-);
+                todo!("download the zip and add the files ig")
             }
         }
     };
